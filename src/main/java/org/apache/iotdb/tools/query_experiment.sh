d1a22bc4b0e1e79387507f2216dbc42312447c45
Lei Rui <1010953854@qq.com>
2022-06-13 00:01:59 +0800

    update query_experiment shell

--- src/main/java/org/apache/iotdb/tools/query_experiment.sh
+++ src/main/java/org/apache/iotdb/tools/query_experiment.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 HOME_PATH=/data/rl/v1
+JAR_RELATIVE_PATH=BallSpeed_testspace/QueryBallSpeed-0.12.4.jar
+
 IOTDB_SBIN_HOME=$HOME_PATH/iotdb-server-0.12.4/sbin
-QUERY_JAR_PATH=$HOME_PATH/BallSpeed_testspace/QueryBallSpeed-0.12.4.jar
+QUERY_JAR_PATH=$HOME_PATH/$JAR_RELATIVE_PATH
 
 echo 3 | sudo tee /proc/sys/vm/drop_caches
 cd $IOTDB_SBIN_HOME
