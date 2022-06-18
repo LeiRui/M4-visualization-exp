05ba18ed60d806a8650edcdbd4b83217449330ab
Lei Rui <1010953854@qq.com>
2022-06-18 14:22:11 +0800

    improve run shell automation level

--- src/main/java/org/apache/iotdb/bash/run-BallSpeed-exp.sh
+++ src/main/java/org/apache/iotdb/bash/run-BallSpeed-exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOME_PATH=/data/rl/v1
+HOME_PATH=/data/rl/v2
 
 ############################
 # extract small data for fast exp
@@ -27,85 +27,41 @@ java OverlapGenerator BallSpeed.csv BallSpeed-O_90_10 0 1 90 10
 
 
 ############################
-# $HOME_PATH
-# ---- BallSpeed
-# $HOME_PATH/dataSpace
-# $HOME_PATH/iotdb-server-0.12.4
-# $HOME_PATH/iotdb-engine-example.properties
-# $HOME_PATH/tool.sh
-# $HOME_PATH/ProcessResult.class
-# $HOME_PATH/ProcessResult.java
-# $HOME_PATH/SumResultUnify.class
-# $HOME_PATH/SumResultUnify.java
-# $HOME_PATH/BallSpeed_testspace
-# ---- WriteBallSpeed-0.12.4.jar [make sure updated][time ns ms value long float type][device measurement path]
-# ---- QueryBallSpeed-0.12.4.jar [make sure updated][time ns ms value long float type][device measurement path]
-# ---- query_experiment.sh [make sure updated][call jar name]
-# [EXP1 EXP2]
-# ---- O_10_10_D_0_0:
-# ---- ---- iotdb-engine-enableCPVtrue.properties
-# ---- ---- iotdb-engine-enableCPVfalse.properties
-# ---- ---- write_data.sh
-# ---- ---- vary_w
-# ---- ---- ---- moc
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- ---- mac
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- ---- cpv
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- vary_tqe
-# ---- ---- ---- moc
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- ---- mac
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- ---- cpv
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# [EXP3]
-# ---- O_0_0_D_0_0:
-# ---- ---- iotdb-engine-enableCPVtrue.properties
-# ---- ---- iotdb-engine-enableCPVfalse.properties
-# ---- ---- write_data.sh
-# ---- ---- fix
-# ---- ---- ---- moc
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- ---- mac
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- ---- ---- cpv
-# ---- ---- ---- ---- change_interval_experiments.sh
-# ---- ---- ---- ---- ProcessResult.class
-# ---- ---- ---- ---- ProcessResult.java
-# ---- O_30_10_D_0_0
-# ---- O_50_10_D_0_0
-# ---- O_70_10_D_0_0
-# ---- O_90_10_D_0_0
-# [EXP4]
-# ---- O_10_10_D_9_10
-# ---- O_10_10_D_29_10
-# ---- O_10_10_D_49_10
-# ---- O_10_10_D_69_10
-# ---- O_10_10_D_89_10
-# [EXP5]
-# ---- O_10_10_D_9_30
-# ---- O_10_10_D_9_50
-# ---- O_10_10_D_9_70
-# ---- O_10_10_D_9_90
-
+# Experimental parameter design:
+# [EXP1] Varying the number of time spans w
+# (1) w: 1,2,5,10,20,50,100,200,500,1000,2000,4000,8000
+# (2) uery range: totalRange
+# (3) overlap percentage: 10%
+# (4) delete percentage: 0%
+# (5) delete time range: 0
+# [EXP2] Varying query time range
+# (1) w: 100
+# (2) query range: k*w*totalRange/(pointNum/chunkSize).
+# - target estimated chunks per interval = k
+# - range = k*w*totalRange/(pointNum/chunkSize)
+# - kMax=(pointNum/chunkSize)/w, that is, range=totalRange.
+# - E.g. k=0.2,0.5,1,2.5,5,12
+# (3) overlap percentage: 10%
+# (4) delete percentage: 0%
+# (5) delete time range: 0
+# [EXP3] Varying chunk overlap percentage
+# (1) w: 100
+# (2) query range: totalRange
+# (3) overlap percentage: 0%, 10%, 30%, 50%, 70%, 90%
+# (4) delete percentage: 0%
+# (5) delete time range: 0
+# [EXP4] Varying delete percentage
+# (1) w: 100
+# (2) query range: totalRange
+# (3) overlap percentage: 10%
+# (4) delete percentage: 0%, 9%, 29%, 49%, 69%, 89%
+# (5) delete time range: 10% of chunk time interval, that is 0.1*totalRange/(pointNum/chunkSize)
+# [EXP5] Varying delete time range
+# (1) w: 100
+# (2) query range: totalRange
+# (3) overlap percentage: 10%
+# (4) delete percentage: 49%
+# (5) delete time range: 10%, 30%, 50%, 70%, 90% of chunk time interval, that is x%*totalRange/(pointNum/chunkSize)
 ############################
 
 ############################
@@ -281,17 +237,17 @@ mkdir moc
 cd moc
 cp $HOME_PATH/ProcessResult.* .
 cp ../../iotdb-engine-enableCPVfalse.properties $HOME_PATH/iotdb-server-0.12.4/conf/iotdb-engine.properties
-./../../../query_experiment.sh 13096043655 100 2 >> result_1.txt
+./../../../query_experiment.sh 13096043655 100 2 >> result_1.txt # 0.2
 java ProcessResult result_1.txt result_1.out ../sumResultMOC.csv
-./../../../query_experiment.sh 26192087310 100 2 >> result_2.txt
+./../../../query_experiment.sh 26192087310 100 2 >> result_2.txt # 0.5
 java ProcessResult result_2.txt result_2.out ../sumResultMOC.csv
-./../../../query_experiment.sh 52384174620 100 2 >> result_3.txt
+./../../../query_experiment.sh 52384174620 100 2 >> result_3.txt # 1
 java ProcessResult result_3.txt result_3.out ../sumResultMOC.csv
-./../../../query_experiment.sh 130960436550 100 2 >> result_4.txt
+./../../../query_experiment.sh 130960436550 100 2 >> result_4.txt # 2.5
 java ProcessResult result_4.txt result_4.out ../sumResultMOC.csv
-./../../../query_experiment.sh 261920873100 100 2 >> result_5.txt
+./../../../query_experiment.sh 261920873100 100 2 >> result_5.txt # 5
 java ProcessResult result_5.txt result_5.out ../sumResultMOC.csv
-./../../../query_experiment.sh 617426057627 100 2 >> result_6.txt
+./../../../query_experiment.sh 617426057627 100 2 >> result_6.txt # 12
 java ProcessResult result_6.txt result_6.out ../sumResultMOC.csv
 
 echo "mac"
@@ -1602,98 +1558,111 @@ cp $HOME_PATH/SumResultUnify.* .
 java SumResultUnify sumResultMOC.csv sumResultMAC.csv sumResultCPV.csv result.csv
 
 #########################
-# sum results
-# [EXP1 EXP2]
-# ---- O_10_10_D_0_0
-# ---- ---- vary_w
-# ---- ---- vary_tqe
-# [EXP3]
-# ---- O_0_0_D_0_0
-# ---- O_30_10_D_0_0
-# ---- O_50_10_D_0_0
-# ---- O_70_10_D_0_0
-# ---- O_90_10_D_0_0
-# [EXP4]
-# ---- O_10_10_D_9_10
-# ---- O_10_10_D_29_10
-# ---- O_10_10_D_49_10
-# ---- O_10_10_D_69_10
-# ---- O_10_10_D_89_10
-# [EXP5]
-# ---- O_10_10_D_49_30
-# ---- O_10_10_D_49_50
-# ---- O_10_10_D_49_70
-# ---- O_10_10_D_49_90
+# export results
 #########################
 # [EXP1]
+# w: 1,2,5,10,20,50,100,200,500,1000,2000,4000,8000
+# query range: totalRange
+# overlap percentage: 10%
+# delete percentage: 0%
+# delete time range: 0
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_0_0
 cd vary_w
 cat result.csv >$HOME_PATH/BallSpeed_testspace/exp1.csv
 
 # [EXP2]
+# w: 100
+# query range: k*w*totalRange/(pointNum/chunkSize).
+# - target estimated chunks per interval = k
+# - range = k*w*totalRange/(pointNum/chunkSize)
+# - kMax=(pointNum/chunkSize)/w, that is, range=totalRange.
+# - E.g. k=0.2,0.5,1,2.5,5,12
+# overlap percentage: 10%
+# delete percentage: 0%
+# delete time range: 0
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_0_0
 cd vary_tqe
 cat result.csv >$HOME_PATH/BallSpeed_testspace/exp2.csv
 
 # [EXP3]
+# w: 100
+# query range: totalRange
+# overlap percentage: 0%, 10%, 30%, 50%, 70%, 90%
+# delete percentage: 0%
+# delete time range: 0
 cd $HOME_PATH/BallSpeed_testspace/O_0_0_D_0_0
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
+cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv #带表头
+
+# 把exp1.csv里的w=100那一行复制到exp3.csv里作为overlap percentage 10%的结果
+# ATTENTION: 这里日后改成自动判断取出那一行参数是100的，而不是写死的行数
+sed -n '8,8p' $HOME_PATH/BallSpeed_testspace/exp1.csv >> $HOME_PATH/BallSpeed_testspace/exp3.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_30_10_D_0_0
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
+# cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_50_10_D_0_0
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_70_10_D_0_0
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_90_10_D_0_0
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp3.csv
 
 # [EXP4]
+# w: 100
+# query range: totalRange
+# overlap percentage: 10%
+# delete percentage: 0%, 9%, 29%, 49%, 69%, 89%
+# delete time range: 10% of chunk time interval, that is 0.1*totalRange/(pointNum/chunkSize)
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_9_10
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
+cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv #带表头
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_29_10
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_49_10
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_69_10
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_89_10
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp4.csv
 
 # [EXP5]
+# w: 100
+# query range: totalRange
+# overlap percentage: 10%
+# delete percentage: 49%
+# delete time range: 10%, 30%, 50%, 70%, 90% of chunk time interval, that is x%*totalRange/(pointNum/chunkSize)
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_49_10
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
+cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv #带表头
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_49_30
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_49_50
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_49_70
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
 
 cd $HOME_PATH/BallSpeed_testspace/O_10_10_D_49_90
 cd fix
-cat result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
+sed -n '2,2p' result.csv >>$HOME_PATH/BallSpeed_testspace/exp5.csv
--- src/main/java/org/apache/iotdb/jarCode/GenerateZhongcheDataset.java
+++ src/main/java/org/apache/iotdb/jarCode/GenerateZhongcheDataset.java
@@ -8,7 +8,9 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.LongSummaryStatistics;
 
 public class GenerateZhongcheDataset {
 
@@ -17,14 +19,21 @@ public class GenerateZhongcheDataset {
         "yyyy-MM-dd'T'HH:mm:ss.SSS"); // 2020-06-09T23:51:07.194+08:00
 
     String dataDir = args[0]; // E.g., /disk/zc_csv
-    String targetSensorName = args[1]; // E.g., ZT765
+    String targetSensorName = args[1]; // E.g., ZT765, ZT17
 
     PrintWriter writer = new PrintWriter(targetSensorName + ".csv");
+    writer.println("Time,root.group_69.1701." + targetSensorName); // header
 
     // iteratively get all files under dataDir
     List<File> res = listAllFiles(dataDir, new ArrayList<>());
 
     // extract the column with the targetSensorName, and attach them together
+    long t1 = -1;
+    long t2 = -1;
+    long delta = -1;
+    long tmax = -1;
+    long lastTimestamp = -1;
+    List<Long> lastTimestamps = new ArrayList<>();
     for (File f : res) {
       BufferedReader reader = new BufferedReader(new FileReader(f));
       String line;
@@ -38,6 +47,9 @@ public class GenerateZhongcheDataset {
           break; // assume only one series contains the sensor name per file
         }
       }
+      long ta = -1;
+      tmax = lastTimestamp;
+      lastTimestamps.add(tmax);
       while ((line = reader.readLine()) != null) {
         splits = line.split(",");
 
@@ -53,15 +65,73 @@ public class GenerateZhongcheDataset {
           timestamp = Long.parseLong(splits[0]);
         }
 
-        writer.print(timestamp);
-        writer.print(",");
-        writer.print(value);
-        writer.println();
+        if (t1 < 0) {
+          t1 = timestamp; // the global first timestamp
+        } else if (t2 < 0) {
+          t2 = timestamp; // the global second timestamp
+        } else if (delta < 0) {
+          delta = t2 - t1;
+        }
+
+        if (ta < 0) {
+          ta = timestamp; // the local first timestamp
+        }
+        if (tmax < 0) { // the first file does not need shifting timestamps
+          writer.print(timestamp);
+          writer.print(",");
+          writer.print(value);
+          writer.println();
+          lastTimestamp = timestamp;
+        } else {
+          long newTimestamp = timestamp + (tmax + delta - ta);
+          writer.print(newTimestamp);
+          writer.print(",");
+          writer.print(value);
+          writer.println();
+          lastTimestamp = newTimestamp;
+        }
       }
       reader.close();
     }
-
     writer.close();
+    System.out.println(lastTimestamps);
+
+    // 检查确认 check the Monotonicity of timestamps
+    BufferedReader reader = new BufferedReader(new FileReader(targetSensorName + ".csv"));
+    reader.readLine(); // skip header
+    long previousTimestamp = -1;
+    List<Long> deltas = new ArrayList<>();
+    List<Float> values = new ArrayList<>();
+    String line;
+    while ((line = reader.readLine()) != null) {
+      String[] splits = line.split(",");
+      long timestamp = Long.parseLong(splits[0]);
+      float value = Float.parseFloat(splits[1]);
+      if (timestamp <= previousTimestamp) {
+        System.out.println("Timestamps are not mononically increasing!");
+        System.out.println(timestamp + "<=" + previousTimestamp);
+      }
+      if (previousTimestamp > 0) {
+        values.add(value);
+
+        long tmp = timestamp - previousTimestamp;
+        deltas.add(tmp);
+        if (tmp == 1 || tmp == 1021502636) {
+          System.out.println(tmp + "ms:" + previousTimestamp + "," + timestamp);
+        }
+      }
+      previousTimestamp = timestamp;
+    }
+    reader.close();
+
+    LongSummaryStatistics stats = deltas.stream()
+        .mapToLong((x) -> x)
+        .summaryStatistics();
+    System.out.println(stats);
+    DoubleSummaryStatistics stats2 = values.stream()
+        .mapToDouble((x) -> x)
+        .summaryStatistics();
+    System.out.println(stats2);
   }
 
   public static List<File> listAllFiles(String path, List<File> fileList) {
