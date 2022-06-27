3d3e69533c437995d053e966c976913686d213f8
Lei Rui <1010953854@qq.com>
2022-06-27 09:42:54 +0800

    install bc if not exists

--- src/main/java/org/apache/iotdb/bash/prepare-all.sh
+++ src/main/java/org/apache/iotdb/bash/prepare-all.sh
@@ -5,6 +5,15 @@ mkdir -p $HOME_PATH
 
 find $M4_VISUALIZATION_EXP -type f -iname "*.sh" -exec chmod +x {} \;
 
+# check bc installed
+REQUIRED_PKG="bc"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
 #====prepare general environment====
 cd $HOME_PATH
 cp $M4_VISUALIZATION_EXP/src/main/java/org/apache/iotdb/tools/tool.sh .
