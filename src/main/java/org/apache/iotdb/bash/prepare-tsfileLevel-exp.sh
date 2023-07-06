77ec06a985dc52879d173db3fe0362472acfddaf
Lei Rui <1010953854@qq.com>
2023-07-06 22:53:53 +0800

    rename unify

--- src/main/java/org/apache/iotdb/bash/prepare-tsfileLevel-exp.sh
+++ src/main/java/org/apache/iotdb/bash/prepare-tsfileLevel-exp.sh
@@ -1,5 +1,5 @@
-M4_VISUALIZATION_EXP=/data1/reply/M4-visualization-exp
-HOME_PATH=/data1/reply/tsfileLevelExp
+M4_VISUALIZATION_EXP=/home/ubuntu/M4-visualization-exp
+HOME_PATH=/home/ubuntu/tsfileLevelExp
 VALUE_ENCODING=PLAIN # RLE for int/long, GORILLA for float/double
 TIME_ENCODING=PLAIN # TS_2DIFF
 COMPRESSOR=UNCOMPRESSED
