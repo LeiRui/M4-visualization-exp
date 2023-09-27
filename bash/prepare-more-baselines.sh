680af07dab2863f4963f4ec8658eb5555f53d438
Lei Rui <1010953854@qq.com>
2023-09-28 00:37:29 +0800

    add run-more-baselines shell

---
+++ bash/prepare-more-baselines.sh
@@ -0,0 +1,90 @@
+M4_VISUALIZATION_EXP=/root/ubuntu/M4-visualization-exp
+HOME_PATH=/root/ubuntu/moreBaselinesExp
+
+VALUE_ENCODING=PLAIN # RLE for int/long, GORILLA for float/double
+TIME_ENCODING=PLAIN # TS_2DIFF
+COMPRESSOR=UNCOMPRESSED
+
+mkdir -p $HOME_PATH
+
+find $M4_VISUALIZATION_EXP -type f -iname "*.sh" -exec chmod +x {} \;
+find $M4_VISUALIZATION_EXP -type f -iname "*.sh" -exec sed -i -e 's/\r$//' {} \;
+
+# check bc installed
+REQUIRED_PKG="bc"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+#====prepare general environment====
+cd $HOME_PATH
+cp $M4_VISUALIZATION_EXP/tools/tool.sh .
+cp $M4_VISUALIZATION_EXP/jars/WriteData-*.jar .
+cp $M4_VISUALIZATION_EXP/jars/QueryData-*.jar .
+cp $M4_VISUALIZATION_EXP/tools/query_experiment.sh .
+$HOME_PATH/tool.sh HOME_PATH $HOME_PATH $HOME_PATH/query_experiment.sh
+scp -r $M4_VISUALIZATION_EXP/iotdb-server-0.12.4 .
+scp -r $M4_VISUALIZATION_EXP/iotdb-cli-0.12.4 .
+cp $M4_VISUALIZATION_EXP/tools/iotdb-engine-example.properties .
+cp $M4_VISUALIZATION_EXP/tools/ProcessResult.java .
+cp $M4_VISUALIZATION_EXP/tools/SumResultUnify.java .
+# remove the line starting with "package" in the java file
+sed '/^package/d' ProcessResult.java > ProcessResult2.java
+rm ProcessResult.java
+mv ProcessResult2.java ProcessResult.java
+# then javac it
+javac ProcessResult.java
+# remove the line starting with "package" in the java file
+sed '/^package/d' SumResultUnify.java > SumResultUnify2.java
+rm SumResultUnify.java
+mv SumResultUnify2.java SumResultUnify.java
+# then javac it
+javac SumResultUnify.java
+
+#====prepare run bash for test dataset====
+cd $HOME_PATH
+cp $M4_VISUALIZATION_EXP/bash/run-more-baseline.sh .
+$HOME_PATH/tool.sh HOME_PATH $HOME_PATH run-more-baseline.sh
+$HOME_PATH/tool.sh DATASET BallSpeed run-more-baseline.sh
+$HOME_PATH/tool.sh DEVICE "root.game" run-more-baseline.sh
+$HOME_PATH/tool.sh MEASUREMENT "s6" run-more-baseline.sh
+$HOME_PATH/tool.sh DATA_TYPE long run-more-baseline.sh
+$HOME_PATH/tool.sh TIMESTAMP_PRECISION ns run-more-baseline.sh
+$HOME_PATH/tool.sh DATA_MIN_TIME 0 run-more-baseline.sh
+$HOME_PATH/tool.sh DATA_MAX_TIME 4259092178974 run-more-baseline.sh
+$HOME_PATH/tool.sh TOTAL_POINT_NUMBER 7193200 run-more-baseline.sh
+$HOME_PATH/tool.sh IOTDB_CHUNK_POINT_SIZE 1000 run-more-baseline.sh
+$HOME_PATH/tool.sh VALUE_ENCODING ${VALUE_ENCODING} run-more-baseline.sh
+$HOME_PATH/tool.sh TIME_ENCODING ${TIME_ENCODING} run-more-baseline.sh
+$HOME_PATH/tool.sh COMPRESSOR ${COMPRESSOR} run-more-baseline.sh
+$HOME_PATH/tool.sh hasHeader false run-more-baseline.sh
+
+#====prepare directory for each dataset====
+datasetArray=("BallSpeed");
+for value in ${datasetArray[@]};
+do
+echo "prepare data directory";
+cd $HOME_PATH
+mkdir $value
+cd $value
+cp $M4_VISUALIZATION_EXP/datasets/$value.csv .
+cp $M4_VISUALIZATION_EXP/tools/OverlapGenerator2.java .
+# remove the line starting with "package" in the java file
+sed '/^package/d' OverlapGenerator2.java > OverlapGeneratorTmp.java
+rm OverlapGenerator2.java
+mv OverlapGeneratorTmp.java OverlapGenerator2.java
+# then javac it
+javac OverlapGenerator2.java
+
+echo "prepare testspace directory";
+cd $HOME_PATH
+mkdir ${value}_testspace
+
+done;
+
+find $HOME_PATH -type f -iname "*.sh" -exec chmod +x {} \;
+
+echo "finish"
--- jarCode/src/main/java/org/apache/iotdb/jarCode/QueryData.java
+++ jarCode/src/main/java/org/apache/iotdb/jarCode/QueryData.java
@@ -15,13 +15,19 @@ public class QueryData {
   // * (2) group by ([tqs,tqe),IntervalLength) => Make sure (tqe-tqs) is divisible by
   // IntervalLength!
   // * (3) NOTE the time unit of interval. Update for different datasets!
-  private static final String queryFormat =
+  private static final String M4_LSM =
       "select min_time(%s), max_time(%s), first_value(%s), last_value(%s), min_value(%s), max_value(%s) "
           + "from %s "
           + "group by ([%d, %d), %d%s)"; // note the time precision unit is also parameterized
 
-  private static final String queryFormat_UDF =
-      "select M4(%1$s,'tqs'='%3$d','tqe'='%4$d','w'='%5$d') from %2$s where time>=%3$d and time<%4$d";
+  private static final String MINMAX_LSM = "select min_value(%s), max_value(%s) " + "from %s "
+      + "group by ([%d, %d), %d%s)"; // note the time precision unit is also parameterized
+
+  private static final String M4_UDF = "select M4(%1$s,'tqs'='%3$d','tqe'='%4$d','w'='%5$d') from %2$s where time>=%3$d and time<%4$d";
+
+  private static final String MINMAX_UDF = "select MinMax(%1$s,'tqs'='%3$d','tqe'='%4$d','w'='%5$d') from %2$s where time>=%3$d and time<%4$d";
+
+  private static final String LTTB_UDF = "select Sample(%1$s,'method'='triangle','k'='%5$d') from %2$s where time>=%3$d and time<%4$d";
 
   public static Session session;
 
@@ -54,20 +60,6 @@ public class QueryData {
     int w = Integer.parseInt(args[6]);
     System.out.println("[QueryData] w=" + w);
 
-    // 选择查询执行算法: 1: MAC, 2: MOC, 3: CPV
-    String approach = args[7].toLowerCase();
-    if (!approach.equals("mac") && !approach.equals("moc") && !approach.equals("cpv")) {
-      throw new IOException("Approach wrong. Only accepts mac/moc/cpv");
-    }
-    System.out.println("[QueryData] approach=" + approach);
-    if (approach.equals("moc")) {
-      System.out.println(
-          "MAKE SURE you have set the enable_CPV as false in `iotdb-engine.properties` for MOC!");
-    } else if (approach.equals("cpv")) {
-      System.out.println(
-          "MAKE SURE you have set the enable_CPV as true in `iotdb-engine.properties` for CPV!");
-    }
-
     long minTime;
     long maxTime;
     long interval;
@@ -76,11 +68,56 @@ public class QueryData {
       interval = (long) Math.ceil((double) (dataMaxTime - dataMinTime) / w);
     } else {
       // randomize between [dataMinTime, dataMaxTime-range]
-      minTime =
-          (long) Math.ceil(dataMinTime + Math.random() * (dataMaxTime - range - dataMinTime + 1));
+      minTime = (long) Math.ceil(
+          dataMinTime + Math.random() * (dataMaxTime - range - dataMinTime + 1));
       interval = (long) Math.ceil((double) range / w);
     }
     maxTime = minTime + interval * w;
+
+    // 选择查询执行算法: 1: MAC, 2: MOC, 3: CPV
+    String approach = args[7].toLowerCase();
+    String sql;
+    switch (approach) {
+      case "mac": // M4-UDF
+        sql = String.format(M4_UDF, measurement, device, minTime, maxTime, w);
+        break;
+      case "cpv": // M4-LSM
+      case "moc": // group-by
+        // MOC and CPV sql use the same sql queryFormat.
+        sql = String.format(M4_LSM, measurement, measurement, measurement, measurement, measurement,
+            measurement, device, minTime, maxTime, interval,
+            timestamp_precision);  // note the time precision unit
+        break;
+      case "minmax":
+        //   private static final String MINMAX_UDF =
+        //      "select MinMax(%1$s,'tqs'='%3$d','tqe'='%4$d','w'='%5$d') from %2$s where time>=%3$d and time<%4$d";
+        sql = String.format(MINMAX_UDF, measurement, device, minTime, maxTime, w);
+        break;
+      case "lttb":
+        //  private static final String LTTB_UDF =
+        //      "select Sample(%1$s,'method'='triangle','k'='%5$d') from %2$s where time>=%3$d and time<%4$d";
+        sql = String.format(LTTB_UDF, measurement, device, minTime, maxTime, 4 * w);
+        break;
+      case "minmax_lsm":
+        // "select min_value(%s), max_value(%s) "
+        //          + "from %s "
+        //          + "group by ([%d, %d), %d%s)"; // note the time precision unit is also parameterized
+        sql = String.format(MINMAX_LSM, measurement, measurement, device, minTime, maxTime,
+            interval, timestamp_precision);  // note the time precision unit
+        break;
+      default:
+        throw new IOException("Approach wrong. Only accepts mac/moc/cpv/minmax/lttb/minmax_lsm");
+    }
+
+    System.out.println("[QueryData] approach=" + approach);
+    System.out.println("[QueryData] sql=" + sql);
+    if (approach.equals("moc")) {
+      System.out.println(
+          "MAKE SURE you have set the enable_CPV as false in `iotdb-engine.properties` for MOC!");
+    } else if (approach.equals("cpv") || approach.equals("minmax_lsm")) {
+      System.out.println(
+          "MAKE SURE you have set the enable_CPV as true in `iotdb-engine.properties` for CPV!");
+    }
 
     session = new Session("127.0.0.1", 6667, "root", "root");
     session.open(false);
@@ -90,30 +127,6 @@ public class QueryData {
     // As M4 already does data reduction, so even the w is very big such as 8000, the returned
     // query result size is no more than 8000*4=32000.
     session.setFetchSize(1000000);
-
-    String sql;
-    if (approach.equals("mac")) {
-      // MAC UDF
-      sql =
-          String.format(queryFormat_UDF, measurement, device, minTime, maxTime, w); // MAC
-    } else {
-      // MOC and CPV sql use the same sql queryFormat.
-      sql =
-          String.format(
-              queryFormat,
-              measurement,
-              measurement,
-              measurement,
-              measurement,
-              measurement,
-              measurement,
-              device,
-              minTime,
-              maxTime,
-              interval,
-              timestamp_precision);  // note the time precision unit
-    }
-    System.out.println("[QueryData] sql=" + sql);
 
     long c = 0;
     long startTime = System.nanoTime();
