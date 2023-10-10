a5caed83d93171fc432c9a07ec611f4d80fc8078
Lei Rui <1010953854@qq.com>
2023-10-10 15:46:39 +0800

    add

--- bash/run-multi-series.sh
+++ bash/run-multi-series.sh
@@ -94,8 +94,9 @@ for nts in 1 2
 do
   echo "number of time series=$nts"
   # Usage: ./query_experiment_multiseries.sh device measurement timestamp_precision dataMinTime dataMaxTime range w approach
-  $HOME_PATH/query_experiment_multiseries.sh ${DEVICE} ${MEASUREMENT} ${TIMESTAMP_PRECISION} ${DATA_MIN_TIME} ${DATA_MAX_TIME} ${FIX_QUERY_RANGE} $nts cpv >> result_${i}.txt
-  java ProcessResult result_${i}.txt result_${i}.out ../sumResultCPV.csv
+  $HOME_PATH/query_experiment_multiseries.sh ${DEVICE} ${MEASUREMENT} ${TIMESTAMP_PRECISION} ${DATA_MIN_TIME} ${DATA_MAX_TIME} ${FIX_QUERY_RANGE} $nts cpv >> sumResultCPV.csv
+  # >> result_${i}.txt
+#  java ProcessResult result_${i}.txt result_${i}.out ../sumResultCPV.csv
   let i+=1
 done
 
--- tools/query_experiment_multiseries.sh
+++ tools/query_experiment_multiseries.sh
@@ -4,28 +4,28 @@ HOME_PATH=/data/rl/v1
 IOTDB_SBIN_HOME=$HOME_PATH/iotdb-server-0.12.4/sbin
 QUERY_JAR_PATH=$HOME_PATH/QueryData-0.12.4.jar
 
-echo 3 | sudo tee /proc/sys/vm/drop_caches
+echo 3 | sudo tee /proc/sys/vm/drop_caches >>/dev/null
 cd $IOTDB_SBIN_HOME
 
 a=1
-echo "rep=$a"
+#echo "rep=$a"
 
 w=100
 
-echo "n_ts=$7"
+#echo "n_ts=$7"
 
 for((i=0;i<a;i++)) do
-    echo $i
-    ./start-server.sh /dev/null 2>&1 &
+#    echo $i
+    ./start-server.sh >>/dev/null 2>&1 &
     sleep 12s
 
     for ((deviceID=1; deviceID<=$7; deviceID++)); # query $7 number of time series simultaneously
     do
       echo "$1${deviceID}"
-      java -jar $QUERY_JAR_PATH "$1${deviceID}" $2 $3 $4 $5 $6 $w $8 false NONE
+      java -jar $QUERY_JAR_PATH "$1${deviceID}" $2 $3 $4 $5 $6 $w $8 false NONE >>/dev/null 2>&1
     done;
 
-    ./stop-server.sh
+    ./stop-server.sh >>/dev/null 2>&1
     echo 3 | sudo tee /proc/sys/vm/drop_caches
     sleep 3s
 done
