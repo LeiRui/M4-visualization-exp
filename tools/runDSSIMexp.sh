f0d0cea9d8a53130e88618436658d778f12da2e4
Lei Rui <1010953854@qq.com>
2023-10-19 14:35:55 +0800

    add

--- tools/runDSSIMexp.sh
+++ tools/runDSSIMexp.sh
@@ -1,7 +1,7 @@
-approachArray=("mac" "cpv" "minmax" "minmax_lsm" "lttb" "rawQuery");
-for approach in ${approachArray[@]};
+for w in 100 200 400 600 1200 2000 3000 4000
 do
-  for w in 100 200 400 600 1200 2000 3000 4000
+  approachArray=("mac" "cpv" "minmax" "minmax_lsm" "lttb" "rawQuery");
+  for approach in ${approachArray[@]};
   do
       python3 /data1/ruilei/M4-visualization-exp/tools/parse.py -i /data1/ruilei/moreBaselinesQueryExp-backup20231017 -a ${approach} -w ${w} -H 100 -tqs 0 -tqe 4259092178974 -o /data1/ruilei/M4-visualization-exp/tools
       echo "target/release/line-density ${w} 100 /data1/ruilei/M4-visualization-exp/tools/ts-${approach}-${w}.csv true 0 4259092178974" >> rustPlot.sh
