ae1ef2c207848a49108c07161cc3d40f2b19cbf3
Lei Rui <1010953854@qq.com>
2025-03-22 22:45:14 +0800

    add

---
+++ bash/prepare-overhead.sh
@@ -0,0 +1,122 @@
+BASE_HOME=/root/ubuntu
+
+M4_VISUALIZATION_EXP=${BASE_HOME}/M4-visualization-exp
+HOME_PATH=${BASE_HOME}/ovearheadExp
+
+VALUE_ENCODING=PLAIN # RLE for int/long, GORILLA for float/double
+TIME_ENCODING=PLAIN # TS_2DIFF
+COMPRESSOR=UNCOMPRESSED
+
+mkdir -p $HOME_PATH
+
+find $M4_VISUALIZATION_EXP -type f -iname "*.sh" -exec chmod +x {} \;
+find $M4_VISUALIZATION_EXP -type f -iname "*.sh" -exec sed -i -e 's/\r$//' {} \;
+
+# check bc installed
+REQUIRED_PKG="bc"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+#====prepare general environment====
+cd $HOME_PATH
+cp $M4_VISUALIZATION_EXP/tools/tool.sh .
+cp $M4_VISUALIZATION_EXP/jars/WriteData-*.jar .
+cp $M4_VISUALIZATION_EXP/jars/WriteUpdateData-*.jar .
+cp $M4_VISUALIZATION_EXP/jars/QueryData-*.jar .
+cp $M4_VISUALIZATION_EXP/tools/query_experiment.sh .
+$HOME_PATH/tool.sh HOME_PATH $HOME_PATH $HOME_PATH/query_experiment.sh
+scp -r $M4_VISUALIZATION_EXP/iotdb-server-0.12.4 .
+scp -r $M4_VISUALIZATION_EXP/iotdb-cli-0.12.4 .
+cp $M4_VISUALIZATION_EXP/tools/iotdb-engine-example.properties .
+cp $M4_VISUALIZATION_EXP/tools/ProcessResult.java .
+cp $M4_VISUALIZATION_EXP/tools/SumResultUnify.java .
+# remove the line starting with "package" in the java file
+sed '/^package/d' ProcessResult.java > ProcessResult2.java
+rm ProcessResult.java
+mv ProcessResult2.java ProcessResult.java
+# then javac it
+javac ProcessResult.java
+# remove the line starting with "package" in the java file
+sed '/^package/d' SumResultUnify.java > SumResultUnify2.java
+rm SumResultUnify.java
+mv SumResultUnify2.java SumResultUnify.java
+# then javac it
+javac SumResultUnify.java
+
+
+#====prepare run bash for train====
+cd $HOME_PATH
+cp $M4_VISUALIZATION_EXP/bash/run-overhead-exp.sh .
+$HOME_PATH/tool.sh HOME_PATH $HOME_PATH run-overhead-exp.sh
+$HOME_PATH/tool.sh DATASET Train run-overhead-exp.sh #
+$HOME_PATH/tool.sh DEVICE "root.group6.d17" run-overhead-exp.sh #
+$HOME_PATH/tool.sh MEASUREMENT "Z765" run-overhead-exp.sh #
+$HOME_PATH/tool.sh DATA_TYPE long run-overhead-exp.sh #
+$HOME_PATH/tool.sh TIMESTAMP_PRECISION ms run-overhead-exp.sh #
+$HOME_PATH/tool.sh DATA_MIN_TIME 1591717867194 run-overhead-exp.sh #
+$HOME_PATH/tool.sh DATA_MAX_TIME 1605706903793 run-overhead-exp.sh #
+$HOME_PATH/tool.sh TOTAL_POINT_NUMBER 127802876 run-overhead-exp.sh #
+$HOME_PATH/tool.sh IOTDB_CHUNK_POINT_SIZE 10000 run-overhead-exp.sh #
+$HOME_PATH/tool.sh VALUE_ENCODING ${VALUE_ENCODING} run-overhead-exp.sh # four dataset value types are the same, so can assign the same encodingType
+$HOME_PATH/tool.sh TIME_ENCODING ${TIME_ENCODING} run-overhead-exp.sh
+$HOME_PATH/tool.sh COMPRESSOR ${COMPRESSOR} run-overhead-exp.sh #
+$HOME_PATH/tool.sh hasHeader true run-overhead-exp.sh #
+cp run-overhead-exp.sh run-Train-overhead.sh
+
+#====prepare run bash for steel====
+cd $HOME_PATH
+cp $M4_VISUALIZATION_EXP/bash/run-overhead-exp.sh .
+$HOME_PATH/tool.sh HOME_PATH $HOME_PATH run-overhead-exp.sh
+$HOME_PATH/tool.sh DATASET Steel run-overhead-exp.sh
+$HOME_PATH/tool.sh DEVICE "root.group2.d7" run-overhead-exp.sh
+$HOME_PATH/tool.sh MEASUREMENT "c60" run-overhead-exp.sh
+$HOME_PATH/tool.sh DATA_TYPE double run-overhead-exp.sh
+$HOME_PATH/tool.sh TIMESTAMP_PRECISION ms run-overhead-exp.sh
+$HOME_PATH/tool.sh DATA_MIN_TIME 1664062557838 run-overhead-exp.sh
+$HOME_PATH/tool.sh DATA_MAX_TIME 1683566109697 run-overhead-exp.sh
+$HOME_PATH/tool.sh TOTAL_POINT_NUMBER 314572100 run-overhead-exp.sh
+$HOME_PATH/tool.sh IOTDB_CHUNK_POINT_SIZE 10000 run-overhead-exp.sh
+$HOME_PATH/tool.sh VALUE_ENCODING ${VALUE_ENCODING} run-overhead-exp.sh # four dataset value types are the same, so can assign the same encodingType
+$HOME_PATH/tool.sh TIME_ENCODING ${TIME_ENCODING} run-overhead-exp.sh
+$HOME_PATH/tool.sh COMPRESSOR ${COMPRESSOR} run-overhead-exp.sh
+$HOME_PATH/tool.sh hasHeader true run-overhead-exp.sh
+cp run-overhead-exp.sh run-Steel-overhead.sh
+
+#====prepare directory for each dataset====
+datasetArray=("Train" "Steel");
+for value in ${datasetArray[@]};
+do
+echo "prepare $value directory, this is for data";
+cd $HOME_PATH
+mkdir $value
+cd $value
+cp $M4_VISUALIZATION_EXP/datasets/$value.csv .
+cp $M4_VISUALIZATION_EXP/tools/OverlapGenerator.java .
+# remove the line starting with "package" in the java file
+sed '/^package/d' OverlapGenerator.java > OverlapGenerator2.java
+rm OverlapGenerator.java
+mv OverlapGenerator2.java OverlapGenerator.java
+# then javac it
+javac OverlapGenerator.java
+cp $M4_VISUALIZATION_EXP/tools/AppendTool.java .
+# remove the line starting with "package" in the java file
+sed '/^package/d' AppendTool.java > AppendTool2.java
+rm AppendTool.java
+mv AppendTool2.java AppendTool.java
+# then javac it
+javac AppendTool.java
+
+
+echo "prepare ${value}_testspace directory, this is for write and query";
+cd $HOME_PATH
+mkdir ${value}_testspace
+
+done;
+
+find $HOME_PATH -type f -iname "*.sh" -exec chmod +x {} \;
+
+echo "finish"
---
+++ bash/run-overhead-exp.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+# generate HOME_PATH workspace by running prepare.sh first
+HOME_PATH=/data/v4
+
+# dataset basic info
+DATASET=BallSpeed # BallSpeed KOB MF03 RcvTime
+DEVICE="root.game"
+MEASUREMENT="s6"
+DATA_TYPE=long # long or double
+TIMESTAMP_PRECISION=ns
+DATA_MIN_TIME=0  # in the corresponding timestamp precision
+DATA_MAX_TIME=617426057626  # in the corresponding timestamp precision
+TOTAL_POINT_NUMBER=1200000
+let TOTAL_TIME_RANGE=${DATA_MAX_TIME}-${DATA_MIN_TIME} # check what if not +1 what the difference
+VALUE_ENCODING=PLAIN
+TIME_ENCODING=PLAIN
+COMPRESSOR=UNCOMPRESSED
+
+# iotdb config info
+IOTDB_CHUNK_POINT_SIZE=100
+
+# exp controlled parameter design
+FIX_W=1000
+FIX_QUERY_RANGE=$TOTAL_TIME_RANGE
+FIX_OVERLAP_PERCENTAGE=10
+FIX_DELETE_PERCENTAGE=49
+FIX_DELETE_RANGE=10
+
+hasHeader=false # default
+
+
+echo 3 |sudo tee /proc/sys/vm/drop_cache
+free -m
+echo "Begin experiment!"
+
+#perlist="20 40 60 80 100"
+perlist="20"
+
+#cp ${DATASET}.csv ${DATASET}-O_0
+
+for per in $perlist
+do
+  MAX_POINTS_WRITE=$((echo scale=0 ; echo ${per}*${TOTAL_POINT_NUMBER}/100) | bc )
+  echo "per=${per}% of ${TOTAL_POINT_NUMBER}, points=${MAX_POINTS_WRITE}"
+
+  cd $HOME_PATH/${DATASET}_testspace
+  mkdir O_10_D_0_0
+  cd O_10_D_0_0
+
+  # prepare IoTDB config properties
+  $HOME_PATH/tool.sh system_dir $HOME_PATH/dataSpace_noMetadata_${per}/${DATASET}_O_10_D_0_0/system ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh data_dirs $HOME_PATH/dataSpace_noMetadata_${per}/${DATASET}_O_10_D_0_0/data ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh wal_dir $HOME_PATH/dataSpace_noMetadata_${per}/${DATASET}_O_10_D_0_0/wal ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh timestamp_precision ${TIMESTAMP_PRECISION} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh unseq_tsfile_size 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh seq_tsfile_size 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh avg_series_point_number_threshold ${IOTDB_CHUNK_POINT_SIZE} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh compaction_strategy NO_COMPACTION ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh enable_unseq_compaction false ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh group_size_in_byte 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh page_size_in_byte 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh rpc_address 0.0.0.0 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh rpc_port 6667 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh time_encoder ${TIME_ENCODING} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh compressor ${COMPRESSOR} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh error_Param 50 ../../iotdb-engine-example.properties
+
+  $HOME_PATH/tool.sh write_m4_lsm false ../../iotdb-engine-example.properties # note this!
+
+  # note
+  # enlarge memory allocation for write when writing
+  $HOME_PATH/tool.sh write_read_schema_free_memory_proportion 6:1:1:2 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh MAX_HEAP_SIZE \"12G\" $HOME_PATH/iotdb-server-0.12.4/conf/iotdb-env.sh
+
+  cp ../../iotdb-engine-example.properties iotdb-engine-noMetadata.properties
+
+  # [write data]
+  echo "Writing data $DATASET (without metadata)"
+  cp iotdb-engine-noMetadata.properties $HOME_PATH/iotdb-server-0.12.4/conf/iotdb-engine.properties
+  cd $HOME_PATH/iotdb-server-0.12.4/sbin
+  ./start-server.sh /dev/null 2>&1 &
+  sleep 8s
+  start_time=$(date +%s%N)
+  # Usage: java -jar WriteData-0.12.4.jar device measurement dataType timestamp_precision total_time_length total_point_number iotdb_chunk_point_size filePath deleteFreq deleteLen timeIdx valueIdx VALUE_ENCODING
+  java -jar $HOME_PATH/WriteData*.jar ${DEVICE} ${MEASUREMENT} ${DATA_TYPE} ${TIMESTAMP_PRECISION} ${TOTAL_TIME_RANGE} ${TOTAL_POINT_NUMBER} ${IOTDB_CHUNK_POINT_SIZE} $HOME_PATH/${DATASET}/${DATASET}-O_10 0 0 0 1 ${VALUE_ENCODING} ${hasHeader}
+  end_time=$(date +%s%N)
+  duration_ns=$((end_time - start_time))
+  echo "write latency of $DATASET (without metadata) for $per is: $duration_ns ns"
+  sleep 5s
+  ./stop-server.sh
+  sleep 5s
+  echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+  ######################################################################
+
+  # prepare IoTDB config properties
+  $HOME_PATH/tool.sh system_dir $HOME_PATH/dataSpace_noMetadata_${per}/${DATASET}_O_10_D_0_0/system ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh data_dirs $HOME_PATH/dataSpace_noMetadata_${per}/${DATASET}_O_10_D_0_0/data ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh wal_dir $HOME_PATH/dataSpace_noMetadata_${per}/${DATASET}_O_10_D_0_0/wal ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh timestamp_precision ${TIMESTAMP_PRECISION} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh unseq_tsfile_size 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh seq_tsfile_size 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh avg_series_point_number_threshold ${IOTDB_CHUNK_POINT_SIZE} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh compaction_strategy NO_COMPACTION ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh enable_unseq_compaction false ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh group_size_in_byte 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh page_size_in_byte 1073741824 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh rpc_address 0.0.0.0 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh rpc_port 6667 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh time_encoder ${TIME_ENCODING} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh compressor ${COMPRESSOR} ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh error_Param 50 ../../iotdb-engine-example.properties
+
+  $HOME_PATH/tool.sh write_m4_lsm true ../../iotdb-engine-example.properties # note this!
+
+  # note
+  # enlarge memory allocation for write when writing
+  $HOME_PATH/tool.sh write_read_schema_free_memory_proportion 6:1:1:2 ../../iotdb-engine-example.properties
+  $HOME_PATH/tool.sh MAX_HEAP_SIZE \"12G\" $HOME_PATH/iotdb-server-0.12.4/conf/iotdb-env.sh
+
+  cp ../../iotdb-engine-example.properties iotdb-engine-Metadata.properties
+
+  # [write data]
+  echo "Writing data $DATASET (without metadata)"
+  cp iotdb-engine-Metadata.properties $HOME_PATH/iotdb-server-0.12.4/conf/iotdb-engine.properties
+  cd $HOME_PATH/iotdb-server-0.12.4/sbin
+  ./start-server.sh /dev/null 2>&1 &
+  sleep 8s
+  start_time=$(date +%s%N)
+  # Usage: java -jar WriteData-0.12.4.jar device measurement dataType timestamp_precision total_time_length total_point_number iotdb_chunk_point_size filePath deleteFreq deleteLen timeIdx valueIdx VALUE_ENCODING
+  java -jar $HOME_PATH/WriteData*.jar ${DEVICE} ${MEASUREMENT} ${DATA_TYPE} ${TIMESTAMP_PRECISION} ${TOTAL_TIME_RANGE} ${TOTAL_POINT_NUMBER} ${IOTDB_CHUNK_POINT_SIZE} $HOME_PATH/${DATASET}/${DATASET}-O_10 0 0 0 1 ${VALUE_ENCODING} ${hasHeader}
+  end_time=$(date +%s%N)
+  duration_ns=$((end_time - start_time))
+  echo "write latency of $DATASET (with metadata) for $per is: $duration_ns ns"
+  sleep 5s
+  ./stop-server.sh
+  sleep 5s
+  echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+done
+
+echo "ALL FINISHED!"
+echo 3 |sudo tee /proc/sys/vm/drop_caches
+free -m
